4622a520f0338f0a04085317da817246d20dfb69
Ben Yap <bwyap@outlook.com>
2019-01-24 12:25:23 +1100

    fix: HomePage template

--- internals/templates/containers/HomePage/index.ts
+++ internals/templates/containers/HomePage/index.ts
@@ -1,7 +1,4 @@
-import ConnectedHomePage, {
-  HomePage,
-  mapDispatchToProps,
-} from './parts/HomePage';
+import ConnectedHomePage, { HomePage } from './parts/HomePage';
 
 export default ConnectedHomePage;
-export { HomePage, mapDispatchToProps };
+export { HomePage };
